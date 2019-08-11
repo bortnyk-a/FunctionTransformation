d38638b3e3e7fea7f470df68e31ff6771e2277d6
uriy <lonsdale341@gmail.com>
2019-08-11 22:19:38 +0300

    added transform

--- Assets/Scripts/Test3DLine.cs
+++ Assets/Scripts/Test3DLine.cs
@@ -1,109 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Vectrosity;
-
+using TMPro;
 public class Test3DLine : MonoBehaviour
 {
+    public Material MaterialPlane;
+    public float Lenght;
     public GameObject CameraMain;
     public int segments;
     public int numberOfLines;
-    public float distanceBetweenLines;
+    private float distanceBetweenLines;
     public bool doLoop = true;
     public Texture2D lineTex;
     public Texture2D frontTex;
     public float lineWidth = 2.0f;
+    public Slider A_Slider;
+    public Slider B_slider;
+    public Slider C_slider;
+    public Slider Number_Slider;
+    public TextMeshPro MeshPro;
     List<Vector3> splinePoints;
-    VectorLine spline;
+    List<Vector3> gridPoints;
+    List<Vector3> XOY_Points;
+    VectorLine Grid;
+    VectorLine XOY_Line;
+    VectorLine Spline;
+    Color colorSpline;
+    Color colorGrid;
+    Color colorXOY;
+    float A;
+    float B;
+    float C;
     float index = 0;
     // Start is called before the first frame update
     void Start()
     {
-        VectorLine.SetCamera3D(CameraMain);
-        VectorLine.SetEndCap("arrow", EndCap.Both, 0, -3 , -3, 3 , lineTex, frontTex, frontTex);
-        var points = new List<Vector3>();
-        // Lines down X axis
-        for (int i = -numberOfLines; i <= numberOfLines; i++)
-        {
-            if (i != 0)
-            {
-                points.Add(new Vector3(i * distanceBetweenLines, -((numberOfLines) * distanceBetweenLines), 0));
-                points.Add(new Vector3(i * distanceBetweenLines, (numberOfLines) * distanceBetweenLines, 0));
-            }
-
-        }
-        // Lines down Z axis
-        for (int i = -numberOfLines; i <= numberOfLines; i++)
-        {
-            if (i != 0)
-            {
-                points.Add(new Vector3(-((numberOfLines) * distanceBetweenLines), i * distanceBetweenLines, 0));
-                points.Add(new Vector3((numberOfLines) * distanceBetweenLines, i * distanceBetweenLines, 0));
-            }
-
-        }
-        var line = new VectorLine("Grid", points,  lineWidth * distanceBetweenLines);
-        line.SetColor(Color.white);
-        line.Draw3D();
+        distanceBetweenLines = Lenght / (2 * numberOfLines);
+        A = 1;
+        B = 0;
+        C = 0;
+        // A_Slider.value = A;
+        // B_slider.value = B;
+        // C_slider.value = C;
+        Number_Slider.value = numberOfLines;
+        gridPoints = new List<Vector3>();
         splinePoints = new List<Vector3>();
-
-
-        //spline = new VectorLine("Grid", new List<Vector3>(7000), 4);
-
-        for (float i = -20; i <= 20; i += 0.05f)
-        {
-            //Debug.Log(index);
-            // spline.points3[index] = new Vector3(i, i * i, 0);
-            //index++;
-            if ((i * i) <= numberOfLines && (i * i) >= -numberOfLines)
-            {
-                splinePoints.Add(new Vector3(i * distanceBetweenLines, i * i * distanceBetweenLines, 0));
-            }
-        }
-        Debug.Log(splinePoints.Count);
-        //spline = new VectorLine("Spline", new List<Vector3>(segments + 1), 6.0f, LineType.Continuous);
-
-
-        var XOY_points = new List<Vector3>();
-        
-       // XOY_points.Add(new Vector3(0, (numberOfLines * distanceBetweenLines), 0));
-       // XOY_points.Add(new Vector3(0, -(numberOfLines * distanceBetweenLines), 0));
-        var XO_line = new VectorLine("XO", new List<Vector3>(), 6 * lineWidth * distanceBetweenLines);
-        XO_line.points3.Add(new Vector3(-(numberOfLines * distanceBetweenLines), 0, 0));
-        XO_line.points3.Add(new Vector3((numberOfLines * distanceBetweenLines), 0, 0));
-        XO_line.endCap = "arrow";
-        XO_line.SetColor(Color.black);
-        XO_line.Draw3D();
-        var YO_line = new VectorLine("YO", new List<Vector3>(), 6 * lineWidth * distanceBetweenLines);
-        YO_line.points3.Add(new Vector3(0, (numberOfLines * distanceBetweenLines), 0));
-        YO_line.points3.Add(new Vector3(0, -(numberOfLines * distanceBetweenLines), 0));
-        YO_line.endCap = "arrow";
-        YO_line.SetColor(Color.black);
-        YO_line.Draw3D();
-        spline = new VectorLine("Spline", splinePoints, 10 * lineWidth * distanceBetweenLines, LineType.Continuous);
-        //spline.texture = lineTex;
-        // spline.continuousTexture = true;
-        spline.joins = Joins.Fill;
-
-        spline.SetColor(Color.red);
-        spline.endCap = "arrow";
-        //spline.MakeSpline(splinePoints.ToArray());
-        Debug.Log(spline.points3.Count);
-        spline.Draw3D();
-        var grid = GameObject.Find("Grid");
-        var graph = GameObject.Find("Spline");
-        var xo = GameObject.Find("XO");
-        var yo = GameObject.Find("YO");
-        var controller= GameObject.Find("Controller");
-        grid.transform.parent = controller.transform;
-        graph.transform.parent = controller.transform;
-        graph.transform.localPosition = new Vector3(0,0,-0.01f);
-        xo.transform.parent = controller.transform;
-        yo.transform.parent = controller.transform;
-        xo.transform.localPosition = new Vector3(0, 0, -0.01f);
-        yo.transform.localPosition = new Vector3(0, 0, -0.01f);
-        grid.transform.localPosition = new Vector3(0, 0, 0);
-
+        XOY_Points = new List<Vector3>();
+        colorGrid = Color.white;
+        colorSpline = Color.red;
+        colorXOY = Color.white;
+        VectorLine.SetCamera3D(CameraMain);
+        VectorLine.SetEndCap("arrow", EndCap.Both, 0, -3, -3, 3, lineTex, frontTex, frontTex);
+        CreateGridList();
+        Created_XOY_Line();
+        CreatedSplineList();
+        Grid = new VectorLine("Grid", gridPoints, lineWidth * distanceBetweenLines);
+        XOY_Line = new VectorLine("XOY_Line", XOY_Points, 8 * lineWidth * distanceBetweenLines);
+        Spline = new VectorLine("Spline", splinePoints, 10 * lineWidth * distanceBetweenLines, LineType.Continuous);
+        SetDraw();
+        SetColorWhite();
 
     }
 
@@ -112,17 +69,141 @@ public class Test3DLine : MonoBehaviour
     {
 
     }
+    public void SetDraw()
+    {
+        string equal = "y=";
+        if (A != 1)
+        {
+            equal += A.ToString();
+        }
+
+        if (B != 0)
+        {
+            if (B > 0)
+            {
+                equal += "(x+" + B.ToString() + ")" + "<sup>2</sup>";
+            }
+            else
+            {
+                equal += "(x" + B.ToString() + ")" + "<sup>2</sup>";
+            }
+
+        }
+        else
+        {
+            equal += "x<sup>2</sup>";
+        }
+        if (C != 0)
+        {
+            if (C > 0)
+            {
+                equal += "+" + C.ToString();
+            }
+            else
+            {
+                equal += C.ToString();
+            }
+
+        }
+        if (A == 0)
+        {
+
+            if (C > 0)
+            {
+                equal = "y=" + C.ToString();
+            }
+            else
+            {
+                equal = "y=" + C.ToString();
+            }
+
+
+        }
+        MeshPro.SetText(equal);
+        Grid.SetColor(colorGrid);
+        Grid.Draw3D();
+
+        XOY_Line.SetColor(colorXOY);
+        XOY_Line.endCap = "arrow";
+        XOY_Line.Draw3D();
+
+        Spline.joins = Joins.Fill;
+        Spline.SetColor(colorSpline);
+        Spline.endCap = "arrow";
+
+        Spline.Draw3D();
+
+        var grid = GameObject.Find("Grid");
+        var graph = GameObject.Find("Spline");
+        var xoy = GameObject.Find("XOY_Line");
+
+        var controller = GameObject.Find("Controller");
+        grid.transform.parent = controller.transform;
+        graph.transform.parent = controller.transform;
+        graph.transform.localPosition = new Vector3(0, 0, -0.015f);
+        xoy.transform.parent = controller.transform;
+        xoy.transform.localPosition = new Vector3(0, 0, -0.01f);
+        grid.transform.localPosition = new Vector3(0, 0, 0);
+    }
+    public void CreatedSplineList()
+    {
+        splinePoints.Clear();
+        for (float i = -numberOfLines; i <= numberOfLines; i += 0.01f)
+        {
+            //Debug.Log(index);
+            // spline.points3[index] = new Vector3(i, i * i, 0);
+            //index++;
+            if (A * Mathf.Pow((i + B), 2) + C <= numberOfLines && A * Mathf.Pow((i + B), 2) + C >= -numberOfLines)
+            {
+                splinePoints.Add(new Vector3(i * distanceBetweenLines, (A * Mathf.Pow((i + B), 2) + C) * distanceBetweenLines, 0));
+            }
+        }
+    }
+    public void Created_XOY_Line()
+    {
+        XOY_Points.Clear();
+        XOY_Points.Add(new Vector3(-(numberOfLines * distanceBetweenLines), 0, 0));
+        XOY_Points.Add(new Vector3((numberOfLines * distanceBetweenLines), 0, 0));
+        XOY_Points.Add(new Vector3(0, -(numberOfLines * distanceBetweenLines), 0));
+        XOY_Points.Add(new Vector3(0, (numberOfLines * distanceBetweenLines), 0));
+
+
+    }
+    public void CreateGridList()
+    {
+        gridPoints.Clear();
+        // Lines down X axis
+        for (int i = -numberOfLines; i <= numberOfLines; i++)
+        {
+            if (i != 0)
+            {
+                gridPoints.Add(new Vector3(i * distanceBetweenLines, -((numberOfLines) * distanceBetweenLines), 0));
+                gridPoints.Add(new Vector3(i * distanceBetweenLines, (numberOfLines) * distanceBetweenLines, 0));
+            }
+
+        }
+        // Lines down Z axis
+        for (int i = -numberOfLines; i <= numberOfLines; i++)
+        {
+            if (i != 0)
+            {
+                gridPoints.Add(new Vector3(-((numberOfLines) * distanceBetweenLines), i * distanceBetweenLines, 0));
+                gridPoints.Add(new Vector3((numberOfLines) * distanceBetweenLines, i * distanceBetweenLines, 0));
+            }
+
+        }
+    }
     public void SetOffset()
     {
         //spline.textureOffset = index;
-        spline.SetColor(Color.red);
-       // spline.joins = Joins.Weld;
-        spline.Draw3D();
-       // index += 0.25f;
+        Spline.SetColor(Color.red);
+        // spline.joins = Joins.Weld;
+        Spline.Draw3D();
+        // index += 0.25f;
     }
     public void SetKoef(float value)
     {
-        spline.points3.Clear();
+        Spline.points3.Clear();
         splinePoints.Clear();
 
         for (float i = -50; i <= 50; i += 0.1f)
@@ -138,10 +219,52 @@ public class Test3DLine : MonoBehaviour
 
         }
 
-        spline.points3 = splinePoints;
+        Spline.points3 = splinePoints;
         //spline = new VectorLine("Spline", new List<Vector3>(segments + 1), 6.0f, LineType.Continuous);
         //spline.MakeSpline(splinePoints.ToArray(), segments, doLoop);
-        spline.SetColor(Color.red);
-        spline.Draw3D();
+        Spline.SetColor(Color.red);
+        Spline.Draw3D();
+    }
+    public void SetNubberOflines(float numberLine)
+    {
+        Debug.Log(numberLine);
+        numberOfLines = (int)numberLine;
+        distanceBetweenLines = Lenght / (2 * numberOfLines);
+        CreateGridList();
+        Created_XOY_Line();
+        CreatedSplineList();
+        SetDraw();
+    }
+    public void Set_A(float a)
+    {
+        A = (float)System.Math.Round(a, 1);
+        CreatedSplineList();
+        SetDraw();
+    }
+    public void Set_B(float b)
+    {
+        B = b;
+        CreatedSplineList();
+        SetDraw();
+    }
+    public void Set_C(float c)
+    {
+        C = c;
+        CreatedSplineList();
+        SetDraw();
+    }
+    public void SetColorGreen()
+    {
+        MaterialPlane.SetColor("_Color", Color.green);
+        colorGrid = Color.white;
+        colorXOY = Color.white;
+        SetDraw();
+    }
+    public void SetColorWhite()
+    {
+        MaterialPlane.SetColor("_Color", Color.white);
+        colorGrid = Color.black;
+        colorXOY = Color.black;
+        SetDraw();
     }
 }
